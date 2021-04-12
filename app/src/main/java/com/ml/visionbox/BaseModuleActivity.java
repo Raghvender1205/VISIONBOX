26e170869f880357e532ecdac538f964dbee3978
Raghvender1205 <raghvender1205@gmail.com>
2021-04-12 17:39:58 +0530

    Initial Release

---
+++ app/src/main/java/com/ml/visionbox/BaseModuleActivity.java
@@ -0,0 +1,4 @@
+package com.ml.visionbox;
+
+public class BaseModuleActivity {
+}
---
+++ app/src/main/java/com/ml/visionbox/ObjectDetectionActivity.java
@@ -0,0 +1,14 @@
+package com.ml.visionbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObjectDetectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object_detection);
+    }
+}
