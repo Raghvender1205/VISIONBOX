6bada0cba5c3969dc2ff40cacf5cd0a3bb56ef40
Raghvender1205 <raghvender1205@gmail.com>
2021-04-12 17:40:22 +0530

    Initial Release

--- app/src/main/java/com/ml/visionbox/BaseModuleActivity.java
+++ app/src/main/java/com/ml/visionbox/BaseModuleActivity.java
@@ -1,4 +1,50 @@
 package com.ml.visionbox;
 
-public class BaseModuleActivity {
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseModuleActivity extends AppCompatActivity {
+    protected HandlerThread mBackgroundThread;
+    protected Handler mBackgroundHandler;
+    protected Handler mUIHandler;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mUIHandler = new Handler(getMainLooper());
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        startBackgroundThread();
+    }
+
+    protected void startBackgroundThread() {
+        mBackgroundThread = new HandlerThread("ModuleActivity");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopBackgroundThread();
+        super.onDestroy();
+    }
+
+    protected void stopBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            Log.e("VISIONBOX", "Error on stopping background thread", e);
+        }
+    }
 }
